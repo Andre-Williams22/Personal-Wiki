3c7450ed387d8c6a1edbf7b32ad8233455ad3c8f
Andre Williams <awjrs22@gmail.com>
2020-05-05 21:14:35 -0700

    adds wikipage with minor fixes and re-clones the site due to errors with database

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from wiki.models import Page
 
 # Create your tests here.
+
+
+class WikiTests(TestCase):
+    def test_detail_page(self):
+        # Instance of user to test the pages
+        user = User.objects.create()
+
+        # Create a test detail page
+        Page.objects.create(title="Test", content="test content", author=user)
+
+        # Making a GET request to the home page
+        res = self.client.get('/')
+
+        # Very a 200 response
+        self.assertEqual(res.status_code, 200)
+        result = res.context['pages']
+
+        # Check if we got out test page
+        self.assertQuerysetEqual(
+            result,
+            ['<Page: Test>']
+        )
+
+    def test_create_page(self):
+        # Instance of user to test the pages
+        user = User.objects.create()
+
+        # Post data to be sent via the form
+        post_data = {
+            'title': 'Test',
+            'content': 'test content',
+            'author': user.id
+        }
+
+        # Request to create a post
+        res = self.client.post('/create/', data=post_data)
+
+        # Verify our response
+        self.assertEqual(res.status_code, 302)
+
+        # Get object to test
+        page_object = Page.objects.get(title='Test')
+
+        # Check that the page object was created in the test db
+        self.assertEqual(page_object.title, 'Test')
